4a39211d91239aa37f1c22333b9bb6b00055c9ab
Radek Kunc <haluzarczlp@gmail.com>
2017-10-28 15:15:57 +0200

    Update sh_plugin.lua

--- clockwork/plugins/icannouncements/plugin/sh_plugin.lua
+++ clockwork/plugins/icannouncements/plugin/sh_plugin.lua
@@ -1,3 +1,8 @@
+--[[
+Copyright © Konrad Porebski
+You may use, edit but you must keep the original credit.
+MIT LICENSE
+--]]
 local PLUGIN = PLUGIN;
 
 Clockwork.kernel:IncludePrefixed("cl_plugin.lua");
